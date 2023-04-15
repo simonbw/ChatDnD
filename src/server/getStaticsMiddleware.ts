b8ad898c533645e4eff0b31879c3f8bd2ba73ee4
Simon Baumgardt-Wellander <simonbw@uw.edu>
2023-04-15 15:54:38 -0700

    static serving stuff

---
+++ src/server/getStaticsMiddleware.ts
@@ -0,0 +1,29 @@
+import express, { RequestHandler } from "express";
+import expressHttpProxy from "express-http-proxy";
+import { getNodeEnv } from "./utils/envUtils";
+
+// Determine the right strategy for static files depending on if we're in dev mode or not
+export function getStaticsMiddleware() {
+  if (getNodeEnv() == "production") {
+    console.log("serving static files from dist/client at /static");
+    return express.static("dist/client");
+  } else {
+    console.log("proxying static files to 127.0.0.1:800");
+    let proxyMiddleware: RequestHandler;
+    try {
+      proxyMiddleware = expressHttpProxy("http://127.0.0.1:8000");
+    } catch (error) {
+      console.error("Failed to create statics proxy", error);
+    }
+
+    const wrappedProxyMiddleware: RequestHandler = (req, res, next) => {
+      try {
+        return proxyMiddleware(req, res, next);
+      } catch (error) {
+        console.error("Error with statics proxy", error);
+      }
+    };
+
+    return wrappedProxyMiddleware;
+  }
+}
